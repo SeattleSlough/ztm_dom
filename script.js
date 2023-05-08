700a7f7e77dcafde9085d0d1effa422b75c76af5
Michael J Maggs <seattleslew@runawayserver.com>
2023-05-08 16:27:37 -0700

    Refactored code

--- script.js
+++ script.js
@@ -20,22 +20,52 @@ var button = document.getElementById( "enter" );
 var input = document.getElementById( "userinput" );
 var ul = document.querySelector( "ul" );
 
-button.addEventListener( "click", function() {
-    if( input.value.length > 0 ) {
-        var li = document.createElement( "li" );
-        li.appendChild( document.createTextNode( input.value ) );
-        ul.appendChild( li );
-        input.value = "";
-    }
-}
-);
+//First Draft of Code pre-refactor:
+// button.addEventListener( "click", function() {
+//     if( input.value.length > 0 ) {
+//         var li = document.createElement( "li" );
+//         li.appendChild( document.createTextNode( input.value ) );
+//         ul.appendChild( li );
+//         input.value = "";
+//     }
+// }
+// );
 
-input.addEventListener( "keydown", function( key ) {
-    if( input.value.length > 0 && key.code === "Enter" ) {
-        var li = document.createElement( "li" );
-        li.appendChild( document.createTextNode( input.value ) );
-        ul.appendChild( li );
-        input.value = "";
+// input.addEventListener( "keydown", function( key ) {
+//     if( input.value.length > 0 && key.code === "Enter" ) {
+//         var li = document.createElement( "li" );
+//         li.appendChild( document.createTextNode( input.value ) );
+//         ul.appendChild( li );
+//         input.value = "";
+//     }
+// }
+// );
+
+
+//Refactoring
+function inputLength() {
+    return input.value.length
+}
+
+function createListElement() {
+    var li = document.createElement( "li" );
+    li.appendChild( document.createTextNode( input.value ) );
+    ul.appendChild( li );
+    input.value = "";
+}
+
+function addListAfterClick() {
+    if( inputLength() > 0 ) {
+        createListElement();
     }
 }
-);
+
+function addListAfterEnter( key ) {
+    if( input.value.length > 0 && key.code === "Enter" ) {
+        createListElement()
+    }
+}
+
+button.addEventListener( "click", addListAfterClick ); //we are pointing to, not invoking function
+
+input.addEventListener( "keydown", addListAfterEnter ); //ibid
