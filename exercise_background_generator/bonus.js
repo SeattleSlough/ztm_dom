44b2197532e0631efb941ccd26c022b8addd6a1b
Michael J Maggs <seattleslew@runawayserver.com>
2023-05-13 19:13:41 -0700

    starting randomize button

--- exercise_background_generator/bonus.js
+++ exercise_background_generator/bonus.js
@@ -1,3 +1,6 @@
+
+
+
 var css = document.querySelector( 'h3' );
 var color1 = document.querySelector( '.color1' );
 var color2 = document.querySelector( '.color2' );
@@ -42,6 +45,7 @@ function pickerHex( array ) {
 function initialPickerColors( left, right ) {
     color1.value = left;
     color2.value = right;
+    css.textContent = propertyString;
 }
 function setGradient() {
     body.style.background = `linear-gradient(to right, ${ color1.value }, ${ color2.value })`;
